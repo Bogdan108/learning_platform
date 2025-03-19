5fc3a54cf53a6deac26de8fd9082b39d06584ca0
Bogdan Luckyanchuk <ibogdan533@gmail.com>
2025-03-19 09:36:38 +0300

    Add app routing

--- lib/src/core/navigation/app_router.dart
+++ lib/src/core/navigation/app_router.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:learning_platform/src/feature/admin/widget/pages/admin_page.dart';
 import 'package:learning_platform/src/feature/authorization/widget/pages/email_page.dart';
 import 'package:learning_platform/src/feature/authorization/widget/pages/login_page.dart';
 import 'package:learning_platform/src/feature/authorization/widget/pages/register_page.dart';
+import 'package:learning_platform/src/feature/courses/model/course.dart';
+import 'package:learning_platform/src/feature/courses/widget/course_page.dart';
+import 'package:learning_platform/src/feature/courses/widget/courses_page.dart';
+import 'package:learning_platform/src/feature/profile/widget/profile_page.dart';
 
 const _defaultFadeTransitionDuration = Duration(milliseconds: 200);
 
@@ -11,7 +16,7 @@ final _rootNavigatorKey = GlobalKey<NavigatorState>();
 class AppRouter {
   static final router = GoRouter(
     navigatorKey: _rootNavigatorKey,
-    initialLocation: '/login',
+    initialLocation: '/admin',
     routes: [
       GoRoute(
         path: '/login',
@@ -51,10 +56,41 @@ class AppRouter {
         ],
       ),
       GoRoute(
-        path: '/home',
+        path: '/admin',
         pageBuilder: (context, state) => CustomTransitionPage<void>(
           key: state.pageKey,
-          child: const HomePage(),
+          child: const AdminHomePage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(opacity: animation, child: child),
+          transitionDuration: _defaultFadeTransitionDuration,
+          reverseTransitionDuration: _defaultFadeTransitionDuration,
+        ),
+      ),
+      GoRoute(
+        path: '/courses',
+        pageBuilder: (context, state) => CustomTransitionPage<void>(
+          key: state.pageKey,
+          child: const CoursesPage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(opacity: animation, child: child),
+          transitionDuration: _defaultFadeTransitionDuration,
+          reverseTransitionDuration: _defaultFadeTransitionDuration,
+        ),
+        routes: [
+          GoRoute(
+            path: '/details',
+            builder: (context, state) {
+              final course = state.extra! as Course;
+              return CourseDetailPage(course: course);
+            },
+          )
+        ],
+      ),
+      GoRoute(
+        path: '/profile',
+        pageBuilder: (context, state) => CustomTransitionPage<void>(
+          key: state.pageKey,
+          child: const ProfilePage(),
           transitionsBuilder: (context, animation, secondaryAnimation, child) =>
               FadeTransition(opacity: animation, child: child),
           transitionDuration: _defaultFadeTransitionDuration,
@@ -63,14 +99,4 @@ class AppRouter {
       ),
     ],
   );
-}
-
-// Простейшая заглушка для главного экрана
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('Home')),
-        body: const Center(child: Text('Welcome Home!')),
-      );
 }
