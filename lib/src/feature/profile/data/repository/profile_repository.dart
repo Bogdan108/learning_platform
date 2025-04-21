8fc72a271425c4b67b9c80a6c87907460b36de2b
Bogdan Luckyanchuk <ibogdan533@gmail.com>
2025-04-21 23:07:12 +0300

    Refactor `Profile` data

--- lib/src/feature/profile/data/repository/profile_repository.dart
+++ lib/src/feature/profile/data/repository/profile_repository.dart
@@ -1,3 +1,4 @@
+import 'package:learning_platform/src/feature/authorization/data/storage/i_organization_storage.dart';
 import 'package:learning_platform/src/feature/authorization/data/storage/token_storage.dart';
 import 'package:learning_platform/src/feature/profile/data/data_source/i_profile_data_source.dart';
 import 'package:learning_platform/src/feature/profile/data/repository/i_profile_repository.dart';
@@ -6,14 +7,20 @@ import 'package:learning_platform/src/feature/profile/model/user.dart';
 class ProfileRepository implements IProfileRepository {
   final IProfileDataSource _dataSource;
   final TokenStorage tokenStorage;
+  final IStorage<String> _orgIdStorage;
 
-  ProfileRepository(
-      {required IProfileDataSource dataSource, required this.tokenStorage})
-      : _dataSource = dataSource;
+  ProfileRepository({
+    required IProfileDataSource dataSource,
+    required this.tokenStorage,
+    required IStorage<String> orgIdStorage,
+  })  : _dataSource = dataSource,
+        _orgIdStorage = orgIdStorage;
 
   String get token => tokenStorage.load() ?? '';
 
   @override
-  Future<User> getUserInfo(String organizationId, String token) =>
-      _dataSource.getUserInfo(organizationId, token);
+  Future<User> getUserInfo() {
+    final organizationId = _orgIdStorage.load();
+    return _dataSource.getUserInfo(organizationId ?? '', token);
+  }
 }
