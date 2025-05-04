ffe9abe23990d53ce6789099f32392e34ef00708
Bogdan Luckyanchuk <ibogdan533@gmail.com>
2025-05-04 18:39:21 +0300

    Refactor Ui widgets for `Task` feature

---
+++ lib/src/feature/task/widget/components/task_tile.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:learning_platform/src/common/widget/custom_snackbar.dart';
+import 'package:learning_platform/src/feature/task/data/repository/tasks_repository.dart';
+import 'package:learning_platform/src/feature/task/model/answer_type.dart';
+import 'package:learning_platform/src/feature/task/model/question_type.dart';
+import 'package:learning_platform/src/feature/task/model/task.dart';
+import 'package:share_plus/share_plus.dart';
+
+class TaskTile extends StatelessWidget {
+  final int number;
+  final Task task;
+  final VoidCallback onDeleteTask;
+  final TasksRepository tasksRepository;
+
+  const TaskTile({
+    required this.number,
+    required this.task,
+    required this.onDeleteTask,
+    required this.tasksRepository,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 24,
+                  height: 24,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    number.toString(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                switch (task.questionType) {
+                  QuestionType.text => Expanded(
+                      child: Text(
+                        task.questionText ?? '',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ),
+                  QuestionType.file => Expanded(
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.insert_drive_file,
+                            color: Colors.blue,
+                          ),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: Text(
+                              task.questionFile ?? 'Прикреплён файл',
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ),
+                          const Spacer(),
+                          // Скачивание файла
+                          GestureDetector(
+                            child: const Icon(Icons.download),
+                            onTap: () async {
+                              CustomSnackBar.showSuccessful(
+                                context,
+                                title: 'Скачиваем ...',
+                              );
+
+                              try {
+                                final filePath =
+                                    await tasksRepository.downloadQuestionFile(
+                                  task.id,
+                                );
+                                final params = ShareParams(
+                                  title: task.questionFile,
+                                  files: [XFile(filePath)],
+                                );
+
+                                await SharePlus.instance.share(params);
+                              } catch (e) {
+                                CustomSnackBar.showError(
+                                  context,
+                                  title: 'Ошибка: $e',
+                                );
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                },
+                const SizedBox(width: 10),
+                GestureDetector(
+                  onTap: onDeleteTask,
+                  child: const Icon(
+                    Icons.delete,
+                    color: Color(0xFFC1121F),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25),
+            switch (task.answerType) {
+              AnswerType.text => Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 4),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                    ),
+                    child: const Row(
+                      children: [
+                        Text('Введите вариант ответа...'),
+                      ],
+                    ),
+                  ),
+                ),
+              AnswerType.file => Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 4),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                    ),
+                    child: const Row(
+                      spacing: 12,
+                      children: [
+                        Icon(
+                          Icons.attach_file,
+                          size: 20,
+                        ),
+                        Text('Прикрепите файл с ответом...'),
+                      ],
+                    ),
+                  ),
+                ),
+              AnswerType.variants => Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: task.answerVariants
+                              ?.map(
+                                (e) => Container(
+                                  margin: const EdgeInsets.only(bottom: 4),
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(
+                                      color: Colors.blue,
+                                      width: 2,
+                                    ),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Text(e),
+                                    ],
+                                  ),
+                                ),
+                              )
+                              .toList() ??
+                          [],
+                    ),
+                  ),
+                ),
+            },
+          ],
+        ),
+      );
+}
--- lib/src/feature/task/widget/task_page.dart
+++ lib/src/feature/task/widget/task_page.dart
@@ -1,113 +1,107 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:learning_platform/src/feature/initialization/widget/dependencies_scope.dart';
-// import 'package:learning_platform/src/feature/task/bloc/tasks_bloc.dart';
-// import 'package:learning_platform/src/feature/task/bloc/tasks_bloc_event.dart';
-// import 'package:learning_platform/src/feature/task/bloc/tasks_bloc_state.dart';
-// import 'package:learning_platform/src/feature/task/data/data_source/tasks_data_source.dart';
-// import 'package:learning_platform/src/feature/task/data/repository/tasks_repository.dart';
-// import 'package:learning_platform/src/feature/task/model/task_request.dart';
-// import 'package:learning_platform/src/feature/task/widget/components/answer_task.dart';
-// import 'package:learning_platform/src/feature/task/widget/components/create_task.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learning_platform/src/common/widget/custom_elevated_button.dart';
+import 'package:learning_platform/src/feature/initialization/widget/dependencies_scope.dart';
+import 'package:learning_platform/src/feature/task/bloc/tasks_bloc.dart';
+import 'package:learning_platform/src/feature/task/bloc/tasks_bloc_event.dart';
+import 'package:learning_platform/src/feature/task/bloc/tasks_bloc_state.dart';
+import 'package:learning_platform/src/feature/task/data/data_source/tasks_data_source.dart';
+import 'package:learning_platform/src/feature/task/data/repository/tasks_repository.dart';
+import 'package:learning_platform/src/feature/task/model/answer_type.dart';
+import 'package:learning_platform/src/feature/task/model/question_type.dart';
+import 'package:learning_platform/src/feature/task/model/task_request.dart';
+import 'package:learning_platform/src/feature/task/widget/components/create_task.dart';
+import 'package:learning_platform/src/feature/task/widget/components/delete_task_dialog.dart';
+import 'package:learning_platform/src/feature/task/widget/components/task_tile.dart';
 
-// class TasksPage extends StatefulWidget {
-//   final String assignmentId;
-//   const TasksPage({ required this.assignmentId, super.key });
+class TasksPage extends StatefulWidget {
+  final String assignmentId;
+  const TasksPage({required this.assignmentId, super.key});
 
-//   @override
-//   State<TasksPage> createState() => _TasksPageState();
-// }
+  @override
+  State<TasksPage> createState() => _TasksPageState();
+}
 
-// class _TasksPageState extends State<TasksPage> {
-//   late final TasksBloc _bloc;
+class _TasksPageState extends State<TasksPage> {
+  late final TasksBloc _bloc;
+  late final TasksRepository tasksRepository;
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     final deps = DependenciesScope.of(context);
-//     _bloc = TasksBloc(
-//       repo: TasksRepository(
-//         dataSource: TasksDataSource(dio: deps.dio),
-//         tokenStorage: deps.tokenStorage,
-//         orgIdStorage: deps.organizationIdStorage,
-//       ),
-//     )..add(TasksBlocEvent.fetch(widget.assignmentId));
-//   }
+  @override
+  void initState() {
+    super.initState();
+    final deps = DependenciesScope.of(context);
+    tasksRepository = TasksRepository(
+      dataSource: TasksDataSource(dio: deps.dio),
+      tokenStorage: deps.tokenStorage,
+      orgIdStorage: deps.organizationIdStorage,
+    );
+    _bloc = TasksBloc(
+      repo: tasksRepository,
+    )..add(TasksBlocEvent.fetch(widget.assignmentId));
+  }
 
-//   @override
-//   Widget build(BuildContext context) => Scaffold(
-//       appBar: AppBar(title: const Text('Задачи')),
-//       body: BlocBuilder<TasksBloc, TasksBlocState>(
-//         bloc: _bloc,
-//         builder: (_, state) => ListView.separated(
-//           itemCount: state.tasks.length + 1,
-//           separatorBuilder: (_,__) => const Divider(color: Colors.grey),
-//           itemBuilder: (_, idx) {
-//             if (idx == state.tasks.length) {
-//               return Padding(
-//                 padding: const EdgeInsets.all(16),
-//                 child: ElevatedButton(
-//                   child: const Text('Добавить задачу'),
-//                   onPressed: () {
-//                     CreateTaskDialog(onSave: (qType, qText, aType, variants) {
-//                       _bloc.add(TasksBlocEvent.create(
-//                         assignmentId: widget.assignmentId,
-//                         req: TaskRequest(
-//                           questionType: qType,
-//                           questionText: qText,
-//                           answerType: aType,
-//                           answerVariants: variants,
-//                         ),
-//                       ),);
-//                     },).show(context);
-//                   },
-//                 ),
-//               );
-//             }
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Задачи')),
+        body: BlocBuilder<TasksBloc, TasksBlocState>(
+          bloc: _bloc,
+          builder: (_, state) => ListView.separated(
+            padding: EdgeInsets.zero,
+            separatorBuilder: (context, index) => const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Divider(color: Colors.blue, height: 1),
+            ),
+            itemCount: state.tasks.length + 1,
+            itemBuilder: (_, index) {
+              if (index == state.tasks.length) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Center(
+                    child: CustomElevatedButton(
+                      title: 'Добавить задачу',
+                      onPressed: () {
+                        CreateTaskDialog(
+                          onSave: ({
+                            required QuestionType questionType,
+                            required AnswerType answerType,
+                            String? questionText,
+                            List<String>? answerVariants,
+                            File? questionFile,
+                          }) {
+                            final req = TaskRequest(
+                              questionType: questionType,
+                              questionText: questionText,
+                              answerType: answerType,
+                              answerVariants: answerVariants,
+                            );
+                            _bloc.add(
+                              TasksBlocEvent.create(
+                                assignmentId: widget.assignmentId,
+                                req: req,
+                              ),
+                            );
+                          },
+                        ).show(context);
+                      },
+                    ),
+                  ),
+                );
+              }
+              final task = state.tasks[index];
 
-//             final t = state.tasks[idx];
-//             return ListTile(
-//               title: t.questionType == 'text'
-//                   ? Text(t.questionText ?? '')
-//                   : const Icon(Icons.insert_drive_file),
-//               subtitle: Text('Ответ: ${t.answerType}'),
-//               onTap: () {
-//                 // choose either answer or evaluate depending on role
-//                 final isTeacher = /* read from profileBloc */;
-//                 if (isTeacher) {
-//                   // show evaluation UI
-//                 } else {
-//                   if (t.answerType == 'text') {
-//                     AnswerTaskDialog(onSubmit: (txt) {
-//                       _bloc.add(TasksBlocEvent.answerText(
-//                         assignmentId: widget.assignmentId,
-//                         taskId: t.id,
-//                         text: txt,
-//                       ),);
-//                     },).show(context);
-//                   } else {
-//                     AnswerTaskDialog(onSubmit: (file) {
-//                       _bloc.add(TasksBlocEvent.answerFile(
-//                         assignmentId: widget.assignmentId,
-//                         taskId: t.id,
-//                         file: file,
-//                       ),);
-//                     },).show(context);
-//                   }
-//                 }
-//               },
-//               trailing: IconButton(
-//                 icon: const Icon(Icons.delete, color: Colors.red),
-//                 onPressed: () {
-//                   // DeleteTaskDialog(onConfirm: () {
-//                   //   _bloc.add(TasksBlocEvent.delete(t.id));
-//                   // },).show(context);
-//                 },
-//               ),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-// }
+              return TaskTile(
+                number: index + 1,
+                task: task,
+                tasksRepository: tasksRepository,
+                onDeleteTask: () => DeleteTaskDialog(
+                  onTapCallback: () => _bloc.add(
+                    TasksBlocEvent.delete(task.id, widget.assignmentId),
+                  ),
+                ).show(context),
+              );
+            },
+          ),
+        ),
+      );
+}
