767c1dad45ce17b86dd77b42c6fb09e903e2ef05
Bogdan Luckyanchuk <ibogdan533@gmail.com>
2025-05-11 19:46:24 +0300

    Implement dynamic routing

--- lib/src/core/root_screen/user_root_screen.dart
+++ lib/src/core/root_screen/user_root_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:learning_platform/src/feature/initialization/widget/dependencies_scope.dart';
+import 'package:learning_platform/src/feature/profile/bloc/profile_bloc.dart';
+import 'package:learning_platform/src/feature/profile/bloc/profile_bloc_state.dart';
+import 'package:learning_platform/src/feature/profile/model/user_role.dart';
 
 class UserRootScreen extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
@@ -11,25 +16,39 @@ class UserRootScreen extends StatelessWidget {
 
   @override
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: navigationShell,
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.done_outline_sharp),
-              label: 'Задания',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Курсы',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Профиль',
-            ),
-          ],
-          currentIndex: navigationShell.currentIndex,
-          onTap: navigationShell.goBranch,
-        ),
-      );
+  Widget build(BuildContext context) {
+    final profileBloc = DependenciesScope.of(context).profileBloc;
+
+    return BlocBuilder<ProfileBloc, ProfileBlocState>(
+      bloc: profileBloc,
+      builder: (context, state) {
+        final userRole = state.profileInfo.role;
+        final isStudent = userRole == UserRole.student;
+
+        return Scaffold(
+          body: navigationShell,
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              if (isStudent)
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.done_outline_sharp),
+                  label: 'Задания',
+                ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Курсы',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Профиль',
+              ),
+            ],
+            currentIndex:
+                isStudent ? navigationShell.currentIndex : navigationShell.currentIndex - 1,
+            onTap: navigationShell.goBranch,
+          ),
+        );
+      },
+    );
+  }
 }
